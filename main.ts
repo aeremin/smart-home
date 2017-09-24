c4f638084882c1221ec0c8fc071b1c0edbb73390
Alexey Eremin <a.eremin.msu@gmail.com>
2017-09-24 23:50:26 +0000

    Fix config path

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ import App from "./app";
 import { Config } from "./config";
 
 // tslint:disable-next-line:no-var-requires
-const configUnparsed: Config = require("config.json");
+const configUnparsed: Config = require("./config.json");
 
 new App(configUnparsed).listen(5000);
