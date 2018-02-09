4c7d99eca65ad27bba7b776bb1037095cfe09bfc
Alexey Eremin <a.eremin.msu@gmail.com>
2018-02-09 23:54:40 +0100

    Fix paths to config

--- ir/main.ts
+++ ir/main.ts
@@ -1,5 +1,5 @@
 import App from "./app";
 
 // tslint:disable-next-line:no-var-requires
-const config = require("./config.json");
+const config = require("../config.json");
 new App(config.authConfig).listen(5001);
