556c960b71f4ab362c01c2f9a1072d2d26eca3aa
Alexey Eremin <a.eremin.msu@gmail.com>
2018-02-09 23:50:44 +0100

    Fix buildability

--- common/middleware.ts
+++ common/middleware.ts
@@ -1,12 +1,13 @@
 import * as basic_auth from "basic-auth";
 import * as express from "express";
-import { Config } from "./config";
+import { AuthConfig } from "./auth_config";
+
 
 function statusPage() {
   return (_: express.Request, res: express.Response) => res.status(200).send("I am alive!");
 }
 
-function basicAuth(config: Config) {
+function basicAuth(config: AuthConfig) {
   return async (req: express.Request, res: express.Response, next: any) => {
     const credentials = basic_auth(req);
     if (credentials && credentials.name == config.username && credentials.pass == config.password)
