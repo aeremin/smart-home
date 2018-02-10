7f1a2e62b2e1a1e2632ddd17844a52b94ac17d55
Alexey Eremin <a.eremin.msu@gmail.com>
2018-02-10 17:59:19 +0100

    Refactor out function for creating express app

--- cloud/app.ts
+++ cloud/app.ts
@@ -1,53 +1,47 @@
 import * as bodyparser from "body-parser";
 import * as express from "express";
-import * as http from "http";
 import * as rp from "request-promise";
 import { AuthConfig } from "../common/auth_config";
 import { basicAuth, statusPage } from "../common/middleware";
 import { CloudConfig } from "./cloud_config";
 
-class App {
-  private app: express.Express = express();
-  private server: http.Server;
+function createApp(authConfig: AuthConfig, cloudConfig: CloudConfig): express.Express {
+  const app: express.Express = express();
+  app.use(bodyparser.json());
+  app.get("/status/", statusPage());
+  app.use(basicAuth(authConfig));
 
-  constructor(private _authConfig: AuthConfig, private _cloudConfig: CloudConfig) {
-    this.app.use(bodyparser.json());
-    this.app.get("/status/", statusPage());
-    this.app.use(basicAuth(_authConfig));
+  const impl = new Implementation(authConfig, cloudConfig);
+  app.post("/", (req, res) => {
+    console.log(JSON.stringify(req.body));
+    const intentName: string = req.body.result.metadata.intentName;
+    const params: any = req.body.result.parameters;
+    if (intentName == "connect_to_projector")
+      impl.connectoToProjector(params.hdmi_source);
+    if (intentName == "disable_power")
+      impl.disablePower(params.device);
+    if (intentName == "enable_power")
+      impl.enablePower(params.device);
 
-    this.app.post("/", (req, res) => {
-      console.log(JSON.stringify(req.body));
-      const intentName: string = req.body.result.metadata.intentName;
-      const params: any = req.body.result.parameters;
-      if (intentName == "connect_to_projector")
-        this.connectoToProjector(params.hdmi_source);
-      if (intentName == "disable_power")
-        this.disablePower(params.device);
-      if (intentName == "enable_power")
-        this.enablePower(params.device);
+    if (intentName == "pause_playback")
+      impl.vlcHttp("/pause");
+    if (intentName == "resume_playback")
+      impl.vlcHttp("/resume");
+    if (intentName == "set_volume_level")
+      impl.vlcHttp(`/set_volume_level/${params.level}`);
+    if (intentName == "set_stream_language")
+      impl.vlcHttp(`/set_stream_language/${params.vlc_stream}/${params.language}`);
 
-      if (intentName == "pause_playback")
-        this.vlcHttp("/pause");
-      if (intentName == "resume_playback")
-        this.vlcHttp("/resume");
-      if (intentName == "set_volume_level")
-        this.vlcHttp(`/set_volume_level/${params.level}`);
-      if (intentName == "set_stream_language")
-        this.vlcHttp(`/set_stream_language/${params.vlc_stream}/${params.language}`);
+    res.status(200).send("Ok!");
+  });
 
-      res.status(200).send("Ok!");
-    });
-  }
+  return app;
+}
 
-  public listen(port: number): void {
-    this.server = this.app.listen(port);
-  }
+class Implementation {
+  constructor(private _authConfig: AuthConfig, private _cloudConfig: CloudConfig) { }
 
-  public stop(): void {
-    this.server.close();
-  }
-
-  private connectoToProjector(hdmiSource: string) {
+  public connectoToProjector(hdmiSource: string) {
     const hdmiSourceToKey: { [key: string]: string } = {
       computer: "KEY_PROG1",
       playstation: "KEY_PROG2",
@@ -58,7 +52,7 @@ class App {
     return this.irSendHttp("hdmi_switch", hdmiSourceToKey[hdmiSource]);
   }
 
-  private enablePower(device: string) {
+  public enablePower(device: string) {
     if (device == "sound system")
       return this.irSendHttp("sound_system", "POWER");
 
@@ -66,7 +60,7 @@ class App {
       return this.irSendHttp("projector", "KEY_POWER");
   }
 
-  private disablePower(device: string) {
+  public disablePower(device: string) {
     if (device == "sound system")
       return this.irSendHttp("sound_system", "POWER");
 
@@ -77,17 +71,17 @@ class App {
     }
   }
 
-  private async irSendHttp(receiver: string, key: string) {
-    const status = await rp.get(`${this._cloudConfig.irUrl}/ir_send/${receiver}/${key}`,
+  public async vlcHttp(path: string) {
+    const status = await rp.post(this._cloudConfig.vlcUrl + path,
       { auth: { username: this._authConfig.username, password: this._authConfig.password } });
-    return JSON.parse(status);
+    return status;
   }
 
-  private async vlcHttp(path: string) {
-    const status = await rp.get(this._cloudConfig.vlcUrl + path,
+  private async irSendHttp(receiver: string, key: string) {
+    const status = await rp.post(`${this._cloudConfig.irUrl}/ir_send/${receiver}/${key}`,
       { auth: { username: this._authConfig.username, password: this._authConfig.password } });
     return JSON.parse(status);
   }
 }
 
-export default App;
+export default createApp;
--- cloud/main.ts
+++ cloud/main.ts
@@ -1,5 +1,5 @@
-import App from "./app";
+import createApp from "./app";
 
 // tslint:disable-next-line:no-var-requires
 const config = require("../config.json");
-new App(config.authConfig, config.cloudConfig).listen(5000);
+createApp(config.authConfig, config.cloudConfig).listen(5000);
