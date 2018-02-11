9efab857a464635b322b3cea7550e5619618c4bd
Alexey Eremin <a.eremin.msu@gmail.com>
2018-02-11 19:26:59 +0100

    Add basic auth

--- cloud_function/implementation.ts
+++ cloud_function/implementation.ts
@@ -1,49 +1,58 @@
-import * as rp from "request-promise";
+import axios, { AxiosResponse } from "axios";
 import { AuthConfig } from "./auth_config";
 import { CloudConfig } from "./cloud_config";
 
 export class Implementation {
   constructor(private _authConfig: AuthConfig, private _cloudConfig: CloudConfig) { }
 
-  public connectoToProjector(hdmiSource: string) {
+  public async connectoToProjector(hdmiSource: string): Promise<AxiosResponse> {
     const hdmiSourceToKey: { [key: string]: string } = {
       computer: "KEY_PROG1",
       playstation: "KEY_PROG2",
       chromecast: "KEY_PROG3",
       nintendo: "KEY_PROG4",
     };
-    this.enablePower("projector");
+    const response = await this.enablePower("projector");
+    if (response.status != 200)
+      return response;
     return this.irSendHttp("hdmi_switch", hdmiSourceToKey[hdmiSource]);
   }
 
-  public enablePower(device: string) {
+  public async enablePower(device: string): Promise<AxiosResponse> {
     if (device == "sound system")
       return this.irSendHttp("sound_system", "POWER");
 
     if (device == "projector")
       return this.irSendHttp("projector", "KEY_POWER");
+
+    throw new RangeError("Unknown device");
   }
 
-  public disablePower(device: string) {
+  public async disablePower(device: string): Promise<AxiosResponse> {
     if (device == "sound system")
       return this.irSendHttp("sound_system", "POWER");
 
     if (device == "projector") {
       const disable = () => this.irSendHttp("projector", "KEY_SUSPEND");
-      disable();
-      setTimeout(disable, 1000);
+      const response = await disable();
+      if (response.status != 200)
+        return response;
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      return await disable();
     }
+
+    throw new RangeError("Unknown device");
   }
 
-  public async vlcHttp(path: string) {
-    const status = await rp.post(this._cloudConfig.vlcUrl + path,
+  public async vlcHttp(path: string): Promise<AxiosResponse> {
+    const status: AxiosResponse = await axios.post(this._cloudConfig.vlcUrl + path, {},
       { auth: { username: this._authConfig.username, password: this._authConfig.password } });
     return status;
   }
 
-  private async irSendHttp(receiver: string, key: string) {
-    const status = await rp.post(`${this._cloudConfig.irUrl}/ir_send/${receiver}/${key}`,
+  private async irSendHttp(receiver: string, key: string): Promise<AxiosResponse> {
+    const status: AxiosResponse = await axios.post(`${this._cloudConfig.irUrl}/ir_send/${receiver}/${key}`, {},
       { auth: { username: this._authConfig.username, password: this._authConfig.password } });
-    return JSON.parse(status);
+    return status;
   }
 }
--- cloud_function/index.ts
+++ cloud_function/index.ts
@@ -1,32 +1,48 @@
+import { AxiosResponse } from "axios";
+import * as basic_auth from "basic-auth";
 import * as express from "express";
 import { Implementation } from "./implementation";
 
+const authConfig = { username: "", password: "" };
+
 const impl = new Implementation(
-  { username: "", password: "" },
+  authConfig,
   {
     irUrl: "",
     vlcUrl: "",
   });
 
 export async function http(req: express.Request, res: express.Response) {
+  const credentials = basic_auth(req);
+  if (!(credentials && credentials.name == authConfig.username && credentials.pass == authConfig.password)) {
+    res.header("WWW-Authentificate", "Basic");
+    res.status(401).send("Access denied");
+    return;
+  }
+
   console.log(JSON.stringify(req.body));
   const intentName: string = req.body.result.metadata.intentName;
   const params: any = req.body.result.parameters;
+
+  let response: AxiosResponse<any>;
+
   if (intentName == "connect_to_projector")
-    await impl.connectoToProjector(params.hdmi_source);
-  if (intentName == "disable_power")
-    await impl.disablePower(params.device);
-  if (intentName == "enable_power")
-    await impl.enablePower(params.device);
+    response = await impl.connectoToProjector(params.hdmi_source);
+  else if (intentName == "disable_power")
+    response = await impl.disablePower(params.device);
+  else if (intentName == "enable_power")
+    response = await impl.enablePower(params.device);
 
-  if (intentName == "pause_playback")
-    await impl.vlcHttp("/pause");
-  if (intentName == "resume_playback")
-    await impl.vlcHttp("/resume");
-  if (intentName == "set_volume_level")
-    await impl.vlcHttp(`/set_volume_level/${params.level}`);
-  if (intentName == "set_stream_language")
-    await impl.vlcHttp(`/set_stream_language/${params.vlc_stream}/${params.language}`);
+  else if (intentName == "pause_playback")
+    response = await impl.vlcHttp("/pause");
+  else if (intentName == "resume_playback")
+    response = await impl.vlcHttp("/resume");
+  else if (intentName == "set_volume_level")
+    response = await impl.vlcHttp(`/set_volume_level/${params.level}`);
+  else if (intentName == "set_stream_language")
+    response = await impl.vlcHttp(`/set_stream_language/${params.vlc_stream}/${params.language}`);
+  else
+    throw RangeError("Unknow intentName");
 
-  res.status(200).send("Ok!");
+  res.status(response.status).send(response.data);
 }
