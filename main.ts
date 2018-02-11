11141131740139cf8b75c17368abeb0d2632e220
Alexey Eremin <a.eremin.msu@gmail.com>
2018-02-11 01:39:36 +0100

    Delete unused stuff

--- main.ts
+++
@@ -1,7 +0,0 @@
-import App from "./app";
-import { Config } from "./config";
-
-// tslint:disable-next-line:no-var-requires
-const configUnparsed: Config = require("./config.json");
-
-new App(configUnparsed).listen(5000);
--- start_ngrok.sh
+++
@@ -1,2 +0,0 @@
- screen -S ngrok ../ngrok http 5000
- # after that I need to copypaste address to api.ai and detach from that screen session (C-a C-d)
